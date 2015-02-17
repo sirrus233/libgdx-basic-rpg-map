67d6728ba77a923991d950c196492a7ad244b89f
Bradley Sherman <sirrus233@gmail.com>
2015-02-17 02:45:24 -0800

    Screen size temporary fix. Should eventually use viewport

--- desktop/src/cf/basicrpg/desktop/DesktopLauncher.java
+++ desktop/src/cf/basicrpg/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package cf.basicrpg.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import cf.basicrpg.BasicRPG;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 500;
+		config.height = 500;
 		new LwjglApplication(new BasicRPG(), config);
 	}
 }
